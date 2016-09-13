055b7d436be7c3435ff79bac0580d920c4ed07e5
tmartinez <tmartinez@switchfly.com>
2016-09-12 21:40:48 -0700

    first commit

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetButton : MonoBehaviour {
+	[SerializeField]
+	private Transform myBall;
+
+	public void ResetBallPosition() {
+		myBall.position = Vector3.up * 0.5f;
+
+		Rigidbody ballBody = myBall.GetComponent<Rigidbody> ();
+
+		ballBody.velocity = Vector3.zero;
+		ballBody.angularVelocity = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throwable : MonoBehaviour {
+	[SerializeField]
+	private float power = 1000.0f;
+
+	private Vector3 gravity = Vector3.down * 20;
+
+	private Vector3 startPosition;
+
+	private Vector3 endPosition;
+
+	private Transform myTransform;
+
+	private Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform> ();
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnMouseDown() {
+		startPosition = Input.mousePosition;
+		startPosition.z = myTransform.position.z - UnityEngine.Camera.main.transform.position.z;
+		startPosition = UnityEngine.Camera.main.ScreenToWorldPoint (startPosition);
+	}
+
+	void OnMouseUp() {
+		endPosition = Input.mousePosition;
+		endPosition.z = myTransform.position.z - UnityEngine.Camera.main.transform.position.z;
+		endPosition = UnityEngine.Camera.main.ScreenToWorldPoint (endPosition);
+
+		Vector3 force = endPosition - startPosition;
+		force.z = force.magnitude;
+
+		myRigidbody.AddForce (force * power);
+	}
+}
